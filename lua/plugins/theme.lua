299a3b1cf6eb66d1bbcae74737acac7596bd9689
awe123343 <awe1233430@hotmail.com>
2025-12-04 00:06:11 -0800

    style(indent/listchar): astro style

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,9 +13,9 @@ return {
         local whitespace_grey = "#3a4447" -- Medium grey for whitespace (more visible than subtle)
 
         -- Fix for Red ListChars (Whitespace/NonText)
-        set_hl(0, "Whitespace", { fg = whitespace_grey })
-        set_hl(0, "NonText", { fg = whitespace_grey })
-        set_hl(0, "SpecialKey", { fg = whitespace_grey })
+        set_hl(0, "Whitespace", { fg = subtle_grey })
+        set_hl(0, "NonText", { fg = subtle_grey })
+        set_hl(0, "SpecialKey", { fg = subtle_grey })
 
         -- Indentation guides - subtle
         set_hl(0, "IblIndent", { fg = subtle_grey })
