5c909455dd914989cb1d9bf0c7cf3fa389b24ad7
Ziqiao Wang <awe1233430@hotmail.com>
2025-12-03 20:41:25 -0800

    chore(java): add runtimes

---
+++ lua/plugins/java.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      root_dir = function(path)
+        -- Prioritize settings.gradle (only exists at true project root)
+        return vim.fs.root(path, {
+          "settings.gradle",
+          "settings.gradle.kts",
+          "gradlew",
+        }) or vim.fs.root(path, {
+          "build.gradle",
+          "build.gradle.kts",
+          "pom.xml",
+          "build.xml",
+        })
+      end,
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-25",
+                path = "/Library/Java/JavaVirtualMachines/zulu-25.jdk/Contents/Home",
+                default = true,
+              },
+              {
+                name = "JavaSE-21",
+                path = "/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home",
+              },
+              {
+                name = "JavaSE-17",
+                path = "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home",
+              },
+              {
+                name = "JavaSE-11",
+                path = "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home",
+              },
+              {
+                name = "JavaSE-1.8",
+                path = "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
