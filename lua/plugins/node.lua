e7056882640e48dd9725b7f826f424ae33678826
Ziqiao Wang <awe1233430@hotmail.com>
2025-12-03 20:41:25 -0800

    feat(node): auto tag

---
+++ lua/plugins/node.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "svelte",
+      "xml",
+    },
+    opts = {},
+  },
+}
