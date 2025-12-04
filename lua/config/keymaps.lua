486b23e05deb632ba53cbe4f5547edd2f53781ef
Ziqiao Wang <awe1233430@hotmail.com>
2025-12-03 20:41:24 -0800

    feat(keymap): q to quit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Quit with 'q'
+vim.keymap.set("n", "q", "<cmd>q<cr>", { desc = "Quit", silent = true })
+
+-- Record macros with 'Q'
+vim.keymap.set("n", "Q", "q", { desc = "Record Macro" })
