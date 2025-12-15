74c4818eea27d0ff02716799fa8e793f957c26fc
awe123343 <awe1233430@hotmail.com>
2025-12-15 15:58:56 -0800

    chore(treesitter): disable bash

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,10 @@ return {
       vim.list_extend(opts.ensure_installed or {}, {
         "comment", -- for TODO/FIXME/HACK highlighting
       })
+      -- Disable Treesitter highlighting for bash/sh/zsh - use Vim's built-in syntax instead
+      opts.highlight = opts.highlight or {}
+      opts.highlight.disable = opts.highlight.disable or {}
+      vim.list_extend(opts.highlight.disable, { "bash" })
     end,
   },
 }
