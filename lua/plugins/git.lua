ccf952228da46375b61f587059ecbe744a8472eb
awe123343 <awe1233430@hotmail.com>
2025-12-17 01:25:11 -0800

    feat(git): add diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,4 +11,31 @@ return {
       current_line_blame_formatter = " <author> • <author_time:%Y-%m-%d %H:%M:%S> • <summary> • <abbrev_sha>",
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { winbar_info = true },
+        file_history = { winbar_info = true },
+        merge_tool = {
+          layout = "diff3_horizontal",
+          disable_diagnostics = true,
+          winbar_info = true,
+        },
+      },
+      hooks = {
+        -- Prevents other UI plugins from messing up the diff view
+        diff_buf_read = function(bufnr)
+          vim.b[bufnr].view_activated = false
+        end,
+      },
+    },
+  },
 }
